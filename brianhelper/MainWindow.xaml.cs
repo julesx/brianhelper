11e00d16e84ff0593971f6e04ec1918573e18925
julesx <jlrosen416@gmail.com>
2015-05-12 16:09:23 -0400

    sdf

---
+++ brianhelper/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace brianhelper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Vegetable> Vegetables { get; set; }
+        public List<Fruit> Fruits { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Vegetables = new List<Vegetable>();
+            Fruits = new List<Fruit>();
+
+            Vegetables.Add(new Vegetable("Carrot"));
+            Vegetables.Add(new Vegetable("Brocoli"));
+
+            Fruits.Add(new Fruit("Apple"));
+            Fruits.Add(new Fruit("Pear"));
+
+            DataContext = this;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Get combobox values!
+        }
+    }
+
+    public class Vegetable
+    {
+        public string Name { get; set; }
+
+        public Vegetable(string name)
+        {
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+    public class Fruit
+    {
+        public string Name { get; set; }
+
+        public Fruit(string name)
+        {
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
